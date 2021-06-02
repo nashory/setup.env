d98bc5027470354d85e6a681997548bca7d7fb97
craig.starr <craig.starr@kakaobrain.com>
2021-06-02 15:16:34 +0900

    .

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ git config --global user.email craig.starr@kakaobrain.com
 mkdir -p ~/.vim/autoload && wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/.vim/autoload/plug.vim && sudo apt-get install ctags && cd ~ && rm -rf setup.env && git clone https://github.com/nashory/setup.env.git && cp setup.env/vimrc ~/.vimrc && vim -E -s -u "~/.vimrc" +PlugInstall +qall
 
 # tmux Setting
-sudo apt-get install -y tmux cmake && cp -r ~/setup.env/tmux-config ~/.tmux && ln -s ~/.tmux/.tmux.conf ~/.tmux.conf && cd ~/.tmux && git submodule init && git submodule update && cd ~/.tmux/vendor/tmux-mem-cpu-load && mkdir -p build && cd build && cmake .. && sudo make install && cd ~ && tmux source-file ~/.tmux.conf
+sudo apt-get install -y tmux cmake && rm -rf ~/.tmux && cp -r ~/setup.env/tmux-config ~/.tmux && ln -s ~/.tmux/.tmux.conf ~/.tmux.conf && cd ~/.tmux && cd ~/.tmux/vendor/tmux-mem-cpu-load && mkdir -p build && cd build && cmake .. && sudo make install && cd ~ && tmux source-file ~/.tmux.conf
